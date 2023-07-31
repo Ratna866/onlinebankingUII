61d75e225588ba3a3bfd44365a628f7a63eda0d5
Patil <ratnmala-gajanan.patil@capgemini.com>
2023-07-31 12:23:20 +0530

    navbar colour is changed in all headers

--- OnlinebankingUI/src/app/components/financial-details/financial-details.component.ts
+++ OnlinebankingUI/src/app/components/financial-details/financial-details.component.ts
@@ -18,16 +18,16 @@ interface FinancialDetails {
   styleUrls: ['./financial-details.component.scss']
 })
 export class FinancialDetailsComponent implements OnInit {
-  financialDetails: FinancialDetails = {
-    accountNumber: '1234567890',
-    balance: 5000,
-    recentTransactions: [
-      { date: '2023-07-26', description: 'Groceries', amount: -50 },
-      { date: '2023-07-25', description: 'Salary', amount: 2000 },
-      { date: '2023-07-24', description: 'Utilities', amount: -150 },
-      { date: '2023-07-23', description: 'ATM Withdrawal', amount: -100 }
-    ]
-  };
+  // financialDetails: FinancialDetails = {
+  //   accountNumber: '',
+  //   balance: ,
+  //   recentTransactions: [
+  //     { date: '', description: '', amount: -50 },
+  //     { date: '', description: '', amount: 2000 },
+  //     { date: '', description: '', amount: -150 },
+  //     { date: '', description: '', amount: -100 }
+  //   ]
+  // };
 
   constructor() { }
 
